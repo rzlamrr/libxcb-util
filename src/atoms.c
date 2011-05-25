69fd3c5ec820dedfa743ea677d0618c63fe292de
Arnaud Fontaine <arnau@debian.org>
2011-05-25 14:08:58 +0900

    Do not rely anymore on gperf and m4 following removal of deprecated atoms.
    As atoms were deprecated by 0a5c59f, gperf and m4 are not needed anymore, thus
    rename files accordingly and downgrade minimum required version of autoconf
    (2.62 was only needed to check for M4 in M4 util macro).
    
    Also, remove xcb_atom_get_predefined() and xcb_atom_get_name_predefined()
    which were only meaningful for those atoms and bump the SONAME accordingly.
    
    Signed-off-by: Arnaud Fontaine <arnau@debian.org>

--- src/atoms.c
+++ src/atoms.c
@@ -1,5 +1,3 @@
-%{
-
 /* Rely on vasprintf (GNU extension) instead of vsnprintf if
    possible... */
 #ifdef HAVE_VASPRINTF
@@ -11,49 +9,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include "xcb_atom.h"
-
-%}
-
-%readonly-tables
-%pic
-%null-strings
-%enum
-%includes
-%compare-strncmp
-
-%struct-type
-struct atom_map { int name; xcb_atom_t value; };
-%%
-define(`COUNT', 0)dnl
-define(`DO', `$1,define(`COUNT', incr(COUNT))COUNT')dnl
-include(atomlist.m4)`'dnl
-%%
-
-static const char atom_names[] =
-define(`DO', `	"$1\0"')dnl
-include(atomlist.m4);
-
-static const uint16_t atom_name_offsets[] = {
-define(`OFFSET', 0)dnl
-define(`DO', `	OFFSET,define(`OFFSET', eval(OFFSET+1+len($1)))')dnl
-include(atomlist.m4)`'dnl
-};
-
-xcb_atom_t xcb_atom_get_predefined(uint16_t name_len, const char *name)
-{
-	const struct atom_map *value = in_word_set(name, name_len);
-	xcb_atom_t ret = XCB_NONE;
-	if(value)
-		ret = value->value;
-	return ret;
-}
-
-const char *xcb_atom_get_name_predefined(xcb_atom_t atom)
-{
-	if(atom <= 0 || atom > (sizeof(atom_name_offsets) / sizeof(*atom_name_offsets)))
-		return 0;
-	return atom_names + atom_name_offsets[atom - 1];
-}
 
 static char *makename(const char *fmt, ...)
 {
